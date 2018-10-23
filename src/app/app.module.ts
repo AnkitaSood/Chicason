f4429a1786812ad817fc9d2830ac073ac85ec2f2
ankita <ankita.s33@gmail.com>
2018-10-23 18:25:57 -0400

    Cleaning up app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
 import {RouterModule} from '@angular/router';
-import {MatListModule} from '@angular/material';
 import {HomeComponent} from './home/home.component';
 import {SideNavModule} from './side-nav/side-nav.module';
 import {HomeModule} from './home/home.module';
@@ -28,12 +22,6 @@ const appRoutes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
     RouterModule.forRoot(appRoutes),
     SideNavModule,
     HomeModule
