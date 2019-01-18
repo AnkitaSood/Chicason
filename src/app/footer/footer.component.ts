fb9574e0f30be415c4aae201edbba8bedd604117
ankitasood <ankita.s33@gmail.com>
2019-01-18 00:21:18 -0500

    Sticky footer and material menu

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['../../../node_modules/font-awesome/css/font-awesome.css', './footer.component.css']
 })
 export class FooterComponent {
   title = 'footer';
