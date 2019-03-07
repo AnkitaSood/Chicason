4fa38a3bfe835756e28ce122faa01bd98e471b4e
ankitasood <ankita.s33@gmail.com>
2019-03-07 11:31:16 -0500

    Color theme update

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,26 +4,24 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DataService {
+  catAPIKey = '5f267a1e-26fa-4a93-96ef-e7d214f9d5f6';
+  dogAPIkey = 'c9f15cec-8894-45af-9dfd-bc2eeb6cbdaf';
 
   constructor(private http: HttpClient) { }
 
   fetchDogs() {
-    return this.http.get('https://api.TheDogAPI.com/v1/breeds', {headers: {'x-api-key': 'c9f15cec-8894-45af-9dfd-bc2eeb6cbdaf'}});
+    return this.http.get('https://api.thedogapi.com/v1/breeds', {headers: {'x-api-key': this.dogAPIkey}});
   }
 
   fetchDogBreedImage(breedId) {
-    return this.http.get('https://api.TheDogAPI.com/v1/images/search?breed_ids=' + breedId + '&limit=1', {headers: {'x-api-key': 'c9f15cec-8894-45af-9dfd-bc2eeb6cbdaf'}});
+    return this.http.get('https://api.thedogapi.com/v1/images/search?breed_ids=' + breedId + '&limit=1', {headers: {'x-api-key': this.dogAPIkey}});
   }
 
   fetchCats() {
-    return this.http.get('https://api.thecatapi.com/v1/breeds', {headers: {'x-api-key': '5f267a1e-26fa-4a93-96ef-e7d214f9d5f6'}});
+    return this.http.get('https://api.thecatapi.com/v1/breeds', {headers: {'x-api-key': this.catAPIKey}});
   }
 
   fetchCatBreedImage(breedId) {
-    return this.http.get('https://api.thecatapi.com/v1/images/search?breed_ids=' + breedId + '&limit=1', {headers: {'x-api-key': '5f267a1e-26fa-4a93-96ef-e7d214f9d5f6'}});
-  }
-
-  fetchImageCategories() {
-    return this.http.get('https://api.thedogapi.com/v1/categories', {headers: {'x-api-key': 'c9f15cec-8894-45af-9dfd-bc2eeb6cbdaf'}});
+    return this.http.get('https://api.thecatapi.com/v1/images/search?breed_ids=' + breedId + '&limit=1', {headers: {'x-api-key': this.catAPIKey}});
   }
 }
